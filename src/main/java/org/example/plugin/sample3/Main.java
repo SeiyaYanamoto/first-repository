102a5e6fbd202e5a8efa7c9b4915fb57c9b9abbd
SeiyaYanamoto <yanamoto.engineer@gmail.com>
2025-10-20 13:26:04 +0900

    first commit

--- src/main/java/org/example/plugin/sample3/Main.java
+++ src/main/java/org/example/plugin/sample3/Main.java
@@ -67,48 +67,6 @@ public void onJoinPlayer(PlayerJoinEvent e){
 }
 
 
-  /**
-   * プレイヤーがスニークを開始/終了する際に起動されるイベントハンドラ。
-   *
-   * @param e イベント
-   */
-  @EventHandler
-  public void onPlayerToggleSneak(PlayerToggleSneakEvent e) throws IOException {
-    // イベント発生時のプレイヤーやワールドなどの情報を変数に持つ。
-    Player player = e.getPlayer();
-    World world = player.getWorld();
-
-    if (count % 2 == 0) {
-
-      // 花火オブジェクトをプレイヤーのロケーション地点に対して出現させる。
-      Firework firework = world.spawn(player.getLocation(), Firework.class);
-
-      // 花火オブジェクトが持つメタ情報を取得。
-      FireworkMeta fireworkMeta = firework.getFireworkMeta();
-
-      // 今回は青色で星型の花火を打ち上げる。
-      fireworkMeta.addEffect(
-          FireworkEffect.builder()
-              .withColor(Color.RED)
-              .withColor(Color.WHITE)
-              .with(Type.BALL_LARGE)
-              .withFlicker()
-              .build());
-      fireworkMeta.setPower(3);
-
-      // 追加した情報で再設定する。
-      firework.setFireworkMeta(fireworkMeta);
-
-      Path path = Path.of("firework.txt");
-      Files.writeString(path, "たまーやー");
-      player.sendMessage(Files.readString(path));
-
-    }
-
-    count++;
-
-  }
-
   /**ログイン時にメッセージが表示されるようになる。
    *
    * @param event
